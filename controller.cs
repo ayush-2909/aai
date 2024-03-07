1aa0aa4a6c71431cd7e9b257b421f9cb28998658
Ayush Prabhu <108335897+ayush-2909@users.noreply.github.com>
2024-03-07 19:46:03 +0530

    Create controller.cs

---
+++ controller.cs
@@ -0,0 +1,111 @@
+using CRUD_application_2.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CRUD_application_2.Controllers
+{
+    public class UserController : Controller
+    {
+        public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
+        // GET: User
+        public ActionResult Index(string searchString)
+        {
+            var users = from u in userlist
+                        select u;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                users = users.Where(s => s.Name.Contains(searchString));
+            }
+
+            return View(users.ToList());
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                userlist.Add(user);
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, User user)
+        {
+            var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                // Update other properties as needed
+
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            userlist.Remove(user);
+            return RedirectToAction("Index");
+        }
+    }
+}
